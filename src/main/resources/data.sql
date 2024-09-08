e16836da750658acd41cf89b3fb1090a8e3d7637
Thiago Rios <thiago.mgoncalves@al.infnet.edu.br.com>
2024-09-08 17:53:28 -0300

    Filtres e Criteria

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO GENRE(name)
+VALUES ('Fantasia');
+
+INSERT INTO GENRE(name)
+VALUES ('Acao');
+
+INSERT INTO GENRE(name)
+VALUES ('Terror');
+
+INSERT INTO PRODUCT(name, genre_id, author, description)
+VALUES ('Eragon', 1, 'Christopher Paolini', 'Sinopse do livro 1');
+
+INSERT INTO PRODUCT(name, genre_id, author, description)
+VALUES ('Jurrassic Park', 3, 'Michael Crichton', 'Sinopse do livro 2');
+
+INSERT INTO PRODUCT(name, genre_id, author, description)
+VALUES ('Eldest', 1, 'Christopher Paolini', 'Sinopse do livro 3');
